07d3d870292343a53e7162f332fb8f20758d9d1a
Artem Posudevskyi <penquin4m@gmail.com>
2024-09-16 15:20:21 +0300

    API works

--- src/moviePage/MoviePage.jsx
+++ src/moviePage/MoviePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function MoviePage() {
     const [movies, setMovies] = useState([]);
@@ -40,25 +40,27 @@ function MoviePage() {
           </ul>
         </nav>
       </header>
-
       {/* Main Content */}
-      <main className="flex-grow p-6">
+      <main className="flex-grow p-6 flex justify-center items-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="bg-white rounded-lg shadow-md p-4">
-            <img
-              src="#"
-              alt="Фильм 1"
-              className="w-full h-48 object-cover rounded-lg"
-            />
-            <div className="mt-4">
-              <h2 className="text-xl font-semibold">Movie 1</h2>
-              <p className="text-gray-600 mt-2">Desc</p>
-            </div>
-          </div>
+          {movies.map((movie, index) => {
+            return (
+              <div key={index} className="bg-white rounded-lg shadow-md p-4">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt="Movie"
+                  className="w-full h-64 object-cover rounded-lg"
+                />
+                <div className="mt-4">
+                  <h2 className="text-xl font-semibold">{movie.title}</h2>
+                  <p className="text-gray-600 mt-2">{movie.overview}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </main>
-
-      {/* Footer */}
+      ;{/* Footer */}
       <footer className="bg-gray-900 p-6 text-white text-center">
         <p>&copy; 2024 All rights reserved. Website about films.</p>
       </footer>
